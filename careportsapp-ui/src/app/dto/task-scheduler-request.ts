aea7feaa5b7d3dc580a455c16d21bb8f2edbdac4
ANUWAS <info.anuwas@gmail.com>
2020-04-15 02:23:17 +0530

    rm

--- careportsapp-ui/src/app/dto/task-scheduler-request.ts
+++
@@ -1,7 +0,0 @@
-export class TaskSchedulerRequest {
-taskStatus:String='All';
-priority:String='All';
-followUpTo:String;
-taskScheduledStartDate:Date;
-taskScheduledEndDate:Date;
-}
--- careportsapp-ui/src/app/entity/release.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Release } from './release';
-
-describe('Release', () => {
-  it('should create an instance', () => {
-    expect(new Release()).toBeTruthy();
-  });
-});
