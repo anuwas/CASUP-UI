73793fb91c3483a5551473380c169e80b4795b29
ANUWAS <info.anuwas@gmail.com>
2020-05-04 00:02:55 +0530

    rm

--- careportsapp-ui/src/app/dto/releasesrcrequest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Releasesrcrequest } from './releasesrcrequest';
-
-describe('Releasesrcrequest', () => {
-  it('should create an instance', () => {
-    expect(new Releasesrcrequest()).toBeTruthy();
-  });
-});
--- careportsapp-ui/src/app/entity/task-scheduler.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TaskScheduler } from './task-scheduler';
-
-describe('TaskScheduler', () => {
-  it('should create an instance', () => {
-    expect(new TaskScheduler()).toBeTruthy();
-  });
-});
